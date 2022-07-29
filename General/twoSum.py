16c9677382011e19573c36b02ae889e5e7d978fc
Eliot <eliotb26@gmail.com>
2022-07-29 00:30:40 -0400

    Desc on twoSum.py

--- General/twoSum.py
+++ General/twoSum.py
@@ -1,3 +1,10 @@
+""" Description:
+Get a list of integers: nums
+get a target value: target
+
+Return a single pair of indeces that sum to that target
+"""
+
 def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_table = {}
         for i in range(len(nums)):
